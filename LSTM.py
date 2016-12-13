3e625620c135223ca175fda4d4639623a5f7095a
selBaez <sel.bs.93@hotmail.com>
2016-12-13 19:41:33 +0100

    LSTM seems to be working, sort of

--- LSTM.py
+++ LSTM.py
@@ -5,26 +5,21 @@ import lasagne
 import argparse
 import numpy as np
 import paths
+import time
+
+FEATURES_DIMENSION = 4096
+CAPTION_DIMENSION = 20 # arbitrary for now
+NUM_UNITS = 10 # arbitrary for now
 
 # run as python LSTM.py --is_train True
 
-def build_rnn(X):
-    l_input = lasagne.layers.InputLayer(shape=X.shape)
+def build_rnn():
+    l_input = lasagne.layers.InputLayer(shape=(None, FEATURES_DIMENSION))
 
-    #not sure if we need dropout
     l_dropout_input = lasagne.layers.DropoutLayer(l_input, p=0.5)
-
-    l_lstm = lasagne.layers.LSTMLayer(l_dropout_input, 100, unroll_scan=True,
-                                  grad_clipping=5.)
+    l_lstm = lasagne.layers.LSTMLayer(l_dropout_input, NUM_UNITS, unroll_scan=False, grad_clipping=5.0)
     l_dropout_output = lasagne.layers.DropoutLayer(l_lstm, p=0.5)
-    l_output = lasagne.layers.DenseLayer(l_dropout_output, nonlinearity=lasagne.nonlinearities.softmax)
-
-    #Discuss structure
-    # l_in = lasagne.layers.InputLayer(shape=(None, None, vocab_size))
-    # l_forward_1 = lasagne.layers.LSTMLayer(l_in, N_HIDDEN, grad_clipping=GRAD_CLIP,nonlinearity=lasagne.nonlinearities.tanh)
-    # l_forward_2 = lasagne.layers.LSTMLayer(l_forward_1, N_HIDDEN, grad_clipping=GRAD_CLIP, nonlinearity=lasagne.nonlinearities.tanh, only_return_final=True)
-    #l_out = lasagne.layers.DenseLayer(l_forward_2, num_units=vocab_size, W = lasagne.init.Normal(), nonlinearity=lasagne.nonlinearities.softmax)
-
+    l_output = lasagne.layers.DenseLayer(l_dropout_output, CAPTION_DIMENSION, nonlinearity=lasagne.nonlinearities.softmax)
 
     return l_output
 
@@ -47,56 +42,75 @@ def main(_):
         train = np.load(paths.PROCESSED_FOLDER + 'merged_train.npy')
         x = train[:, 0]
         y = train[:, 1]
-        #y_train, X_val, y_val, X_test, y_test = load_dataset()
     else:
         print("----------------------------- Loading validation data -----------------------------")
         train = np.load(paths.PROCESSED_FOLDER + 'merged_val.npy')
         x = train[:, 0]
         y = train[:, 1]
 
-    print('Placeholder images')
+    print('Placeholder variables')
     # Prepare Theano variables for inputs and targets
     input_var = T.tensor4('inputs')
     target_var = T.ivector('targets')
 
     print('Build rnn')
-    # Create model
-    network = build_rnn(x)
+    # Input layer
+    l_input = lasagne.layers.InputLayer(shape=(None, None, FEATURES_DIMENSION))
+    # Get shape
+    batch_size, seq_length, _ = l_input.input_var.shape
+    # RNN layer
+    l_lstm = lasagne.layers.LSTMLayer(l_input, num_units=NUM_UNITS)
+    # Flatten
+    l_shp = lasagne.layers.ReshapeLayer(l_lstm, (-1, NUM_UNITS))
+    # Fully connected layer
+    l_dense = lasagne.layers.DenseLayer(l_shp, num_units=CAPTION_DIMENSION, nonlinearity=lasagne.nonlinearities.softmax)
+    # Get original shape back
+    l_output = lasagne.layers.ReshapeLayer(l_dense, (batch_size, seq_length, CAPTION_DIMENSION))
 
+    print('Build prediction and loss')
     #Loss
-    prediction = lasagne.layers.get_output(network)
+    output = lasagne.layers.get_output(l_output)
+    prediction = output[:, -1]
     loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
     loss = loss.mean()
 
+    print('Build optimizer')
     #updates
-    params = lasagne.layers.get_all_params(network, trainable=True)
+    params = lasagne.layers.get_all_params(l_output, trainable=True)
     updates = lasagne.updates.nesterov_momentum(
             loss, params, learning_rate=0.01, momentum=0.9)
-    #this or adagrad???
     #updates = lasagne.updates.adagrad(loss, params, LEARNING_RATE)
 
-    test_prediction = lasagne.layers.get_output(network, deterministic=True)
+    print('Build prediction and loss for testing')
+    test_output = lasagne.layers.get_output(l_output, deterministic=True)
+    test_prediction = test_output[:, -1]
     test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
                                                             target_var)
     test_loss = test_loss.mean()
+
+    print('Build accuracy for testing')
     test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
                       dtype=theano.config.floatX)
 
+    print('----------------------------- Training -----------------------------')
     # Compile a function performing a training step on a mini-batch
-    train_fn = theano.function([input_var, target_var], loss, updates=updates)
+    train_fn = theano.function([l_input.input_var, target_var], loss, updates=updates)
+
+    print('hiiiiii, jeijei')
 
     # Compile a second function computing the validation loss and accuracy:
-    val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
-
+    val_fn = theano.function([l_input.input_var, target_var], loss)
 
     print("Starting training...")
 
     # Iterate over epochs:
-    for epoch in range(num_epochs):
+    for epoch in range(100):
+        print("Epoch ", epoch)        
         train_err = 0
         train_batches = 0
         start_time = time.time()
-        for batch in iterate_minibatches(X_train, y_train, 500, shuffle=True):
+        for batch in iterate_minibatches(x, y, 500, shuffle=True):
+            print("Iterating batch ...")
             inputs, targets = batch
             train_err += train_fn(inputs, targets)
             train_batches += 1
